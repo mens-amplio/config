1927ba0c2d875a1c66647137ea5ae0120bdf44c3
sarah t <sarah.tappon@gmail.com>
2013-05-22 09:26:31 -0700

    checking that file exists in startup script

--- startup.sh
+++ startup.sh
@@ -5,8 +5,14 @@ path=/home/pi/pulse-test/pulse.py
 
 grep_string=$(python -c "import os.path; p='$path'; print '['+p[0]+']'+p[1:]")
 
-if ! ps aux | grep $grep_string > /dev/null ;
+if [ -f $path ]
 then
-	sudo python $path &
+	if ! ps aux | grep $grep_string > /dev/null ;
+	then
+		sudo python $path &
+	else
+		echo Script already running.
+	fi
+else
+	echo Script does not exist. Please edit the path in startup.sh.
 fi
- 
