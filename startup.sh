768121042bd7a2ddede56e3443046aabb74c94dc
sarah t <sarah.tappon@gmail.com>
2013-08-06 19:33:50 -0700

    running OPC and led_plaything on startup

--- startup.sh
+++ startup.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
-
-# full path to the script to be run whenever the pi user logs in
-path=/home/pi/pulse-test/pulse.py
-
-grep_string=$(python -c "import os.path; p='$path'; print '['+p[0]+']'+p[1:]")
-
-if [ -f $path ]
-then
-	if ! ps aux | grep $grep_string > /dev/null ;
-	then
-		sudo python $path &
-	else
-		echo Script already running.
-	fi
-else
-	echo Script does not exist. Please edit the path in startup.sh.
-fi
+sudo /home/pi/openpixelcontrol/bin/ws2801_server > /dev/null 2> /dev/null &
+sleep 2
+cd /home/pi/mens-amplio
+sudo ./led_plaything.py > /dev/null 2> /dev/null &
